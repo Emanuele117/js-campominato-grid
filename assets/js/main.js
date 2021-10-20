7ce49b3fc66228528d90764bdf88d074f1db3eb7
Emanuele117 <emanuele.pica7@gmail.com>
2021-10-20 15:55:32 +0200

    griglia di gioco 1 completata

---
+++ assets/js/main.js
@@ -0,0 +1,43 @@
+//L'utente indica un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata
+
+//in cui ogni cella contiene un numero tra quelli compresi in un range:
+
+/*
+
+con difficoltà 1 => tra 1 e 100
+con difficoltà 2 => tra 1 e 81
+con difficoltà 3 => tra 1 e 49
+
+*/
+
+// seleziono un elemento della DOM container
+
+const containerOne = document.querySelector('.container_difficolta1');
+
+// al click su bottone difficoltà 1 genero una griglia da 1 a 100 con ciclo for
+
+const bottone1 = document.getElementById('bottone1');
+
+bottone1.addEventListener('click', function () {
+    for (let i = 0; i < 100; i++) {
+        // crreo l'elemento dom da mettere nella griglia
+        const divElementOne = document.createElement('div');
+
+        //creo una classe da aggiungere al contenitore appena creato
+        divElementOne.className = 'grid_cell_one'
+
+        // appendo l'elemento creato al container
+        containerOne.append(divElementOne)
+
+        //attacco un eventListner a ciascun elemento creato nel ciclo
+        divElementOne.addEventListener('click', function () {
+            // modifico il colore di questo elemento con style
+            this.style.backgroundColor = 'blue'
+        })
+
+    }
+})
+
+
+
+
